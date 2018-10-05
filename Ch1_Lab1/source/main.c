f0b6de71e9ecc533754cfbcd218c003221f4b4df
107360212 <t107360212@ntut.org.tw>
2018-10-05 17:23:04 +0800

    Ch1

---
+++ Ch1_Lab1/source/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+main() {
+	printf("welcome to c!\n");
+	system("pause");
+	return 0;
+}
---
+++ Ch2/source/User_defined.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include<stdlib.h>
+User_defined_print() {
+	printf("welcome to c!\n");
+}
